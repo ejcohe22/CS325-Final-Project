f8ba3f2b6a4580f39aebfee73ff4a802d3bf460f
Erik Cohen <ejcohe22@colby.edu>
2022-01-22 20:51:20 -0500

    filters and such

--- Final Project/projects.php
+++ Final Project/projects.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/theme.css" type="text/css"/>
     <link rel="icon" type="image/x-icon" href="assets/clover_favicon.ico" />
-    <link rel="stylesheet" href="project.css" type="text/css"/>
+    <link rel="stylesheet" href="./css/project.css" type="text/css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="js/theme.js"></script>
     <!-- <script src= ></script> -->
@@ -36,68 +36,86 @@
     <i class="fa fa-bars fa-3x" id="mobile-menu"></i>
 </div>
 
-<div id="projects">
-    <!-- for every 3 projects-->
-    <div class="row">
-        <!-- for every project -->
-        <div class="card secondary">
-            <h2>Project Name<h2>
-            <pre>Team-members Summary
-                Database
-                Backend
-                Frontend 
-                Description
-            </pre>
-        </div>
-        <div class="card secondary">
-            <h2>Project Name<h2>
-            <pre>Team-members 
-                Summary
-                Database
-                Backend
-                Frontend 
-                Description
-            </pre>
-        </div>
-        <div class="card secondary">
-            <h2>Project Name<h2>
-            <pre>Team-members 
-                Summary
-                Database
-                Backend
-                Frontend 
-                Description
-            </pre>
-        </div>
-    </div>
-</div>
+<button id="filters" > Filter <i class="fa fa-filter"></i></button>
 
 <div class="filters">
-    <form class="secondary">
-        <fieldset>
-            <legend>Filter Projects</legend>
-            <p> holding ctl allows for multiple selections </p>
-            <label for="db">Database Type:</label>
-            <select name="db" id="db" multiple>
+    <form class="secondary">  <!-- $('input[type=checkbox]') -->
+    
+        <fieldset> 
+            <legend>Filter Projects <i class="fa fa-chevron-left" id="close-filter"></i></legend>
+            <h2>Database:<h2>
+            <div id ="db" class="multi-dropdown">
                 <!-- populate with db values using php-->
-              <option value="sql">sql</option>
-            </select>
+                <input type="checkbox" id="mysql" name="mysql" value="db-mysql">
+                <label for="mysql"> MySQL</label><br>
+            </div>
             <br />
-            <label for="backend">Backend:</label>
-            <select name="backend" id="backend" multiple>
+            <h2>Backend:<h2>
+            <div id ="backend" class="multi-dropdown">
                 <!-- populate with db values using php-->
-              <option value="php">php</option>
-            </select>
+                <input type="checkbox" id="php" name="php" value="backend-php">
+                <label for="php">PHP</label><br>
+            </div>
             <br />
-            <label for="frontend">Frontend:</label>
-            <select name="frontend" id="frontend" multiple>
+            <h2>Frontend:<h2>
+            <div id ="frontend" class="multi-dropdown">
                 <!-- populate with db values using php-->
-              <option value="php">php</option>
-            </select>
+                <input type="checkbox" id="html" name="html" value="frontend-html">
+                <label for="html">HTML5</label><br>
+            </div>
             <br />
         </fieldset>
     </form>
 </div>
+
+<div id="projects">
+    <!-- for every 3 projects-->
+    <div id="grid">
+        <!-- for every project -->
+        <div class="card secondary">
+            <h2>Project Name<h2>
+            <pre>Team-members
+                Summary
+                Database
+                Backend
+                Frontend 
+                Description
+            </pre>
+        </div>
+        <div class="card secondary">
+            <h2>Project Name<h2>
+            <pre>Team-members 
+                Summary
+                Database
+                Backend
+                Frontend 
+                Description
+            </pre>
+        </div>
+        <div class="card secondary">
+            <h2>Project Name<h2>
+            <pre>Team-members 
+                Summary
+                Database
+                Backend
+                Frontend 
+                Description
+            </pre>
+        </div>
+        <div class="card secondary">
+            <h2>Project Name<h2>
+                <pre>Team-members 
+                    Summary
+                    Database
+                    Backend
+                    Frontend 
+                    Description
+                </pre>
+        </div>
+    </div>
+</div>
+
+
 
 
 
