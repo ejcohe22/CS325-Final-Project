68c1e76bbdb6a1478fc5366a3c2a3e9e43951a9c
Erik Cohen <ejcohe22@colby.edu>
2022-01-23 19:50:15 -0500

    form js coming together

--- Final Project/js/new_project.js
+++ Final Project/js/new_project.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
+    $("#add_backend").click(function(){
+        var structure = '<div><input class="project-backend" type="text" placeholder="Enter a back-end tool used" /> <i class="fa fa-times-circle delete-field"> remove</i><br /></div>';
+        $("#back_end_tools").append(structure);
+    });
+
     $("#add_frontend").click(function(){
-        var structure = '<input class="project-frontend" type="text" placeholder="Enter a front-end tool used" /> <i class="fa fa-times-circle delete-field"> remove</i><br />';
+        var structure = '<div><input class="project-frontend" type="text" placeholder="Enter a front-end tool used" /> <i class="fa fa-times-circle delete-field"> remove</i><br /></div>';
         $("#front_end_tools").append(structure);
     });
 
-    $(".delete-field").click(function(){
-        console.log("click");
+    $(document).on('click', '.delete-field', function(){
+        $(this).parent().remove(); 
     });
 
     $("form").submit(function( event ) {
