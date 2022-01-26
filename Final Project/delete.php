9d9109b60129b70b4efc651548de75d7c21627e8
Erik Cohen <ejcohe22@colby.edu>
2022-01-26 09:40:55 -0500

    actually delete stuff...

--- Final Project/delete.php
+++ Final Project/delete.php
@@ -1,6 +1,9 @@
 <?php
     if( isset($_POST['id']) ){
         $db = new PDO("mysql:dbname=ProjectCollection;host=localhost", "sam", "blueMooN#101");
+        $db->query("DELETE FROM ProjectBackend WHERE prj_id=" . $_POST['id']);
+        $db->query("DELETE FROM ProjectFrontend WHERE prj_id=" . $_POST['id']);
+        $db->query("DELETE FROM ProjectDeveloper WHERE prj_id=" . $_POST['id']);
         $db->query("DELETE FROM projects WHERE id=" . $_POST['id']);
     }
 ?>
